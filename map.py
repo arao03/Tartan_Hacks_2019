9bec20f01e85bd372df5d3443bc4b1f2c5bd8e5f
Amanda <steiner.amanda@yahoo.com>
2019-02-08 23:48:56 -0500

    + working on scene

--- map.py
+++ map.py
@@ -2,12 +2,32 @@
 SPRITE_OFFSETS = (90, 240)
 SPRITE_LOCATION_LEFT = (40,60)
 SPRITE_LOCATION_RIGHT = (410,60)
+MOUSE_VIS = 0
+SCREEN_SIZE = (540, 300)
 
 # ANNABELLE VARS
 ANNABELLE_PATH = './Assets/Sprites/annabelleexpsheet.png'
 ANNABELLE_EXPRESSIONS = 7
 
+# KAYLIN VARS
+KAYLIN_PATH = './Assets/Sprites/kaylinexpsheet.png'
+KAYLIN_EXPRESSIONS = 8
+
+# FORVIK VARS
+FORVIK_PATH = './Assets/Sprites/forvikexpsheet.png'
+FORVIK_EXPRESSIONS = 6
+
+# EXTRAS
+ELF_PATH = './Assets/Sprites/elfextrasheet.png'
+HUMAN_PATH = './Assets/Sprites/humanextrasheet.png'
+DWARF_PATH = './Assets/Sprites/dwarfextrasheet.png' # not yet created
+
+# TEXT BOX VARS
+TEXTBOX_PATH = './Assets/text_box.png'
+TEXTBOX_RECT = (10, 230), (520, 60)
+
 # BACKGROUNDS
 BACKGROUND_CITY = './Assets/Backgrounds/opencity.png'
 BACKGROUND_HOUSE = './Assets/Backgrounds/house_background.png'
 BACKGROUND_FORGE = './Assets/Backgrounds/shop_background.png'
+BACKGROUND_SCHOOL = './Assets/Backgrounds/school_background.png'
--- scene.py
+++ scene.py
@@ -1,4 +1,6 @@
-import pygame
+import os, sys, pygame
+from pygame.locals import *
+import map
 
 class SpriteSheet(object):
     def __init__(self, filename):
@@ -28,8 +30,6 @@ class SpriteSheet(object):
         tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
                 for x in range(image_count)]
         return self.images_at(tups, colorkey)
-    
-    
 
 class SpriteStripAnim(object):
     """sprite strip animator
@@ -76,3 +76,32 @@ class SpriteStripAnim(object):
     def __add__(self, ss):
         self.images.extend(ss.images)
         return self
+
+class Background(pygame.surface.Surface):
+    def __init__(self, image_file):
+        pygame.surface.Surface.__init__(self, map.SCREEN_SIZE)
+        self.image = pygame.image.load(image_file).convert()
+        self.rect = self.image.get_rect()
+        #self.rect.left, self.rect.top = location
+        
+class TextBox(pygame.surface.Surface):
+    def __init__(self):
+        self.image = pygame.image.load(map.TEXTBOX_PATH).convert()
+        self.rect = pygame.Rect(map.TEXTBOX_RECT)
+        self.image.set_colorkey(self.image.get_at((0,0)))
+        
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, file_name, expression_count, offsets = None):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = SpriteSheet(file_name).load_strip(pygame.Rect((0,0), map.SPRITE_OFFSETS),  expression_count, colorkey = -1)
+        self.expression = 0 # Default
+        
+        self.rect = pygame.Rect(map.SPRITE_LOCATION_LEFT, map.SPRITE_OFFSETS)
+        self.image = self.images[self.expression]
+        
+    def chg_expression(self, expression):
+        self.expression = expression
+        
+    def update(self, expression):
+        self.image = self.images[self.expression]
